d5dd9106cdf678c65637e1178048842a5300de1a
Austin Rivas <austinrivas@gmail.com>
2020-05-17 16:47:59 +0700

    added pattern to create local dirs and bundle assets in binary

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
+use dirs;
 use dotfiles::shell::Command;
+use std::fs;
 use std::time;
 use std::thread;
+use std::path::Path;
+
+
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "assets/"]
+struct Asset;
 
 fn main() {
-    let cmd = String::from("echo");
-    let args = vec!["hello"];
+    let test_sh = Asset::get("test.sh").unwrap();
+    println!("{:?}", std::str::from_utf8(test_sh.as_ref()));
 
-    let task = Command::new(&cmd, &args);
-    let output = task.run();
+    for file in Asset::iter() {
+        println!("{}", file.as_ref());
+    }
 
-    output.log();
+    let mut home_path = match dirs::home_dir() {
+        Some(path) => path,
+        None => panic!("home_dir is not defined in this environment")
+    };
 
-    assert!(output.success());
+    home_path.push("some/dir");
+
+
+    fs::create_dir_all(&home_path)
+        .expect("could not create dir");
+
+    println!("created directory: {:?}", &home_path);
+
+    assert!(Path::new(&home_path).exists());
 
     let cmd = String::from("ping");
-    let args = vec!["-c 10", "www.google.com"];
+    let args = vec!["www.google.com"];
 
     let stream = Command::new(&cmd, &args);
     let mut process = stream.stream();
@@ -24,4 +46,14 @@ fn main() {
 
     let result = process.kill();
     assert!(result.is_ok());
+
+    let cmd = String::from("echo");
+    let args = vec!["hello"];
+
+    let task = Command::new(&cmd, &args);
+    let output = task.run();
+
+    output.log();
+
+    assert!(output.success());
 }
